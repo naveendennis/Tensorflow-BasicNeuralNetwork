d96e861a410f648ada9b78249896176cb31cd6c1
Barnabas <nbarnaba@cci026gq18aws.dyn.uncc.edu>
2017-10-22 12:26:36 -0400

    Initial commit

---
+++ BasicNeuralNetwork.py
@@ -0,0 +1,10 @@
+class BasicNeuralNetwork():
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+
+    def train_neural_network(self):
+        pass
+
+    def evaluate(self):
+        pass
